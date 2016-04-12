c54936b195dea764ef19f138024f0960e695bd76
aquelito <axel@aquelito.fr>
2016-04-12 22:17:17 +0200

    Module Address : add phone link

--- address/templates/address.tpl.php
+++ address/templates/address.tpl.php
@@ -8,7 +8,7 @@
   <br /><span itemprop="postalCode"><?php print $postale_code; ?></span> <span itemprop="addressLocality"><?php print $locality; ?></span></p>
   <p><?php print t('Email'); ?>&nbsp;: <span><a itemprop="email" href="mailto:<?php print $email; ?>"><?php print $email; ?></a></span></p>
   <?php if (isset($phone)) : ?>
-    <p><?php print t('Phone'); ?>&nbsp;: <span itemprop="telephone"><?php print $phone; ?></span></p>
+    <p><?php print t('Phone'); ?>&nbsp;: <a href="<?php print $phone_link; ?>" itemprop="telephone"><?php print $phone; ?></a></p>
   <?php endif; ?>
   <?php if (isset($fax)) : ?>
     <p><?php print t('Fax'); ?>&nbsp;: <span itemprop="faxNumber"><?php print $fax; ?></span></p>
