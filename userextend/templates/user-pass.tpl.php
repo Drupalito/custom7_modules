fb7c9b91b4e7c8af131c3db036ac5633d7e40cb4
aquelito <axel@aquelito.fr>
2016-07-31 17:37:01 +0200

    User templates class

--- userextend/templates/user-pass.tpl.php
+++ userextend/templates/user-pass.tpl.php
@@ -8,9 +8,9 @@
  */
 ?>
 <?php if (render($intro_text)): ?>
-<p class="form__intro"><?php print render($intro_text); ?></p>
+<p class="form-item form-item__intro"><?php print render($intro_text); ?></p>
 <?php endif; ?>
 <?php if (render($intro_required)): ?>
-<p class="form__description"><?php print render($intro_required); ?></p>
+<p class="form-item form-item__description"><?php print render($intro_required); ?></p>
 <?php endif; ?>
 <?php print drupal_render_children($form); ?>
