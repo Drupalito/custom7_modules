ce6809110e05345179505ac13d1b1a161785f61f
aquelito <axel@aquelito.fr>
2017-09-17 05:23:05 +0200

    Change icon fontawesome

--- scrolltotop/scrolltotop.js
+++ scrolltotop/scrolltotop.js
@@ -2,7 +2,7 @@
 (function ($, window, Drupal) {
   Drupal.behaviors.scrolltotop = {
     attach: function (context, settings) {
-      $('body').append('<a href="#" id="scrollToTop" class="scrollToTop"><span aria-hidden="true" class="i i-' + settings.scrolltotop.icon + '"></span><span class="hide">' + settings.scrolltotop.label + '</span></a>');
+      $('body').append('<a href="#" id="scrollToTop" class="scrollToTop scroll"><span aria-hidden="true" class="fa ' + settings.scrolltotop.icon + '"></span><span class="a11y">' + settings.scrolltotop.label + '</span></a>');
       var $scrolltoTop = $('#scrollToTop');
       $(window).scroll(function() {
         ($(this).scrollTop() > settings.scrolltotop.offset) ? $scrolltoTop.addClass('scrollToTop-visible') : $scrolltoTop.removeClass('scrollToTop-visible');
