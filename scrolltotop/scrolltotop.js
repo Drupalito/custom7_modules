b2afec8b927d17712449924bbe9a6ab837bc7138
aquelito <axel@aquelito.fr>
2018-01-27 08:32:00 +0100

    Scrolltotop: change button html

--- scrolltotop/scrolltotop.js
+++ scrolltotop/scrolltotop.js
@@ -2,7 +2,7 @@
 (function ($, window, Drupal) {
   Drupal.behaviors.scrolltotop = {
     attach: function (context, settings) {
-      $('body').append('<a href="#" id="scrollToTop" class="scrollToTop scroll"><span aria-hidden="true" class="fa ' + settings.scrolltotop.icon + '"></span><span class="a11y">' + settings.scrolltotop.label + '</span></a>');
+      $('body').append('<button id="scrollToTop" class="scrollToTop scroll"><span aria-hidden="true" class="fa ' + settings.scrolltotop.icon + '"></span><span class="a11y">' + settings.scrolltotop.label + '</span></button>');
       var $scrolltoTop = $('#scrollToTop');
       $(window).scroll(function() {
         ($(this).scrollTop() > settings.scrolltotop.offset) ? $scrolltoTop.addClass('scrollToTop-visible') : $scrolltoTop.removeClass('scrollToTop-visible');
