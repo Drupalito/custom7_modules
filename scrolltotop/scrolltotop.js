bd520163387b83bf0bebb98a138f2eca2e7784be
aquelito <axel@aquelito.fr>
2018-11-06 18:17:06 +0100

    Fix of the year

--- scrolltotop/scrolltotop.js
+++ scrolltotop/scrolltotop.js
@@ -2,7 +2,7 @@
 (function ($, window, Drupal) {
   Drupal.behaviors.scrolltotop = {
     attach: function (context, settings) {
-      $('body').append('<button id="scrollToTop" class="scrollToTop scroll"><span aria-hidden="true" class="fa ' + settings.scrolltotop.icon + '"></span><span class="a11y">' + settings.scrolltotop.label + '</span></button>');
+      $('body').append('<button id="scrollToTop" class="scrollToTop scroll noprint"><span aria-hidden="true" class="fa ' + settings.scrolltotop.icon + '"></span><span class="a11y">' + settings.scrolltotop.label + '</span></button>');
       var $scrolltoTop = $('#scrollToTop');
       $(window).scroll(function() {
         ($(this).scrollTop() > settings.scrolltotop.offset) ? $scrolltoTop.addClass('scrollToTop-visible') : $scrolltoTop.removeClass('scrollToTop-visible');
